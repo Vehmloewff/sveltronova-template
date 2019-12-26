0d36a1f94f045c8137287357cfcece43ecb015b1
Elijah <ejsisgreat@gmail.com>
2019-12-26 12:34:27 -0600

    Add a setup script

---
+++ scripts/prevent-auto-open.js
@@ -0,0 +1,12 @@
+const { readFileSync, writeFileSync } = require("fs");
+
+const path = `./platforms/browser/cordova/lib/run.js`;
+
+let data = readFileSync(path, "utf-8");
+
+data = data.replace(
+  `return server.launchBrowser({ 'target': args.target, 'url': projectUrl });`,
+  `return server;`
+);
+
+writeFileSync(path, data, "utf-8");
---
+++ setup.sh
@@ -0,0 +1,2 @@
+./node_modules/.bin/cordova prepare
+node ./scripts/prevent-auto-open.js
